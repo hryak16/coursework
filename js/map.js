5126b0fb3b5993802a177827e04833fbad8a5ffb
hryak16 <91027347+hryak16@users.noreply.github.com>
2022-12-01 15:43:31 +0300

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,26 @@
+let myMap;
+
+const init = () => {
+    myMap =new ymaps.Map("map",{
+        center:  [59.94851657, 30.26590746],
+        zoom: 15,
+        controls: []
+    });
+const coords = [
+    [59.94851657, 30.26590746]
+];
+const myCollection = new ymaps.GeoObjectCollection({}, {
+    draggble: false,
+    iconLayout: 'default#image',
+    iconImageHref: '/img/метка.svg',
+    iconImageSize: [100, 100],
+    iconImageOffset: [-46, -66]
+});
+coords.forEach(coord => {
+    myCollection.add(new ymaps.Placemark(coord));
+})
+myMap.geoObjects.add(myCollection);
+
+myMap.behaviors.disable('scrollZoom');
+}
+ymaps.ready(init);
